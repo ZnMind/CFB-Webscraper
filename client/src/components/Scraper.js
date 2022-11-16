567c7102067756e66898938406dd4710b78699d9
DKProtocol <damann1072@gmail.com>
2022-11-16 10:44:10 -0600

    Updating

--- client/src/components/Scraper.js
+++ client/src/components/Scraper.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Pagination from "./Pagination";
 
 const Scraper = props => {
     const [winner, setWinner] = useState([])
@@ -11,21 +10,23 @@ const Scraper = props => {
 
     useEffect(() => {
         console.log(props.url)
-        setLoad("Loading...")
-        getScrape();
-    }, [props])
+        
+        fetch(`https://vercel-webscraping-server-znmind.vercel.app/`)
+            .then(res => res.json())
+            .then(body => console.log(body))
+    }, [])
 
     const getScrape = () => {
         setLoad("Loading...")
         setScraped(false)
-        fetch(`http://localhost:5000/scrape?value=${props.url}&value=.Table tbody tr .teams__col a`)
+        fetch(`https://vercel-webscraping-server-znmind.vercel.app/scrape?value=${props.url}&value=.Table tbody tr .teams__col a`)
             .then(res => res.json())
             .then(body => {
                 setWinner(body.data)
                 return body.data;
             })
             .then(() => {
-                fetch(`http://localhost:5000/scrape?value=${props.url}&value=.Table tbody tr td .Table__Team`)
+                fetch(`https://vercel-webscraping-server-znmind.vercel.app/scrape?value=${props.url}&value=.Table tbody tr td .Table__Team`)
                     .then(res => res.json())
                     .then(body => {
                         setTeam(body.data)
@@ -35,41 +36,11 @@ const Scraper = props => {
                         }
                         return newArr;
                     })
-                    .then(data => {
-                        //setTeam(newArr);
-                    })
             })
             .catch(err => {
                 setLoad("Try Again")
                 console.log(err)
             })
-    }
-
-    const getTeams = () => {
-        setLoad("Loading...")
-        setScraped(false)
-        fetch(`http://localhost:5000/scrape?value=https://www.espn.com/college-football/schedule/_/week/5/year/2022/seasontype/2&value=.Table tbody tr td .Table__Team`)
-            .then(res => res.json())
-            .then(body => {
-                setTeam(body.data)
-                setScraped(true)
-            })
-            .catch(err => {
-                setLoad("Try Again")
-                console.log(err)
-            })
-    }
-
-    const getL = () => {
-        console.log(winner.length)
-        console.log(team.length)
-        newArr[1] = winner
-        for (let i = 0; i < winner.length; i++) {
-            newArr[0][i] = `${team[i * 2]} - ${team[i * 2 + 1]}`;
-        }
-        console.log(newArr)
-        //setData(newArr)
-        return newArr;
     }
 
     for (let i = 0; i < winner.length; i++) {
@@ -83,21 +54,11 @@ const Scraper = props => {
     return (
         <div>
             <div>
-                {/* <Button
-                    text="Win"
+                <Button
+                    text="Scrape"
                     onClick={getScrape}
                     className="btn btn-primary"
                 />
-                <Button
-                    text="Scrape"
-                    onClick={getTeams}
-                    className="btn btn-primary"
-                />
-                <Button
-                    text="Get"
-                    onClick={getL}
-                    className="btn btn-primary"
-                /> */}
             </div>
             <div className="h-container">
                 <div className="column">
--- client/src/components/Select.js
+++ client/src/components/Select.js
@@ -9,12 +9,12 @@ const options = [
     { value: "Week 4", label: "Week 4" },
     { value: "Week 5", label: "Week 5" },
     { value: "Week 6", label: "Week 6" },
-    /* { value: "Week 7", label: "Week 7" },
+    { value: "Week 7", label: "Week 7" },
     { value: "Week 8", label: "Week 8" },
     { value: "Week 9", label: "Week 9" },
     { value: "Week 10", label: "Week 10" },
     { value: "Week 11", label: "Week 11" },
-    { value: "Week 12", label: "Week 12" } */
+    //{ value: "Week 12", label: "Week 12" }
 ]
 
 const MyComponent = () => {
